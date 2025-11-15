18ba52973c8fa93519410ec6ae6f2d997cbb2e60
Luis Gonzalez <codewithluislg@gmail.com>
2025-11-14 23:22:18 -0600

    server updated

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import userRouter from "./routes/user.router.js";
-import db from "../src/config/db.js";
+import db from "./config/db.js";
 import colors from "colors";
 import cors from "cors";
 import { corsConfig } from "./config/cors.js";
